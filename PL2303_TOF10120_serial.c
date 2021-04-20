e2bba26f63eff78a070cda56cb4b7a75d5c81321
sigurd-dev <66842802+sigurd-dev@users.noreply.github.com>
2021-04-20 10:38:38 +0200

    Add files via upload

--- PL2303_TOF10120_serial.c
+++ PL2303_TOF10120_serial.c
@@ -30,37 +30,6 @@
 //For string replacement.
 #include "strrep.h"
 
-int replacestr(char *line, const char *search, const char *replace)
-{
-   int count;
-   char *sp; // start of pattern
-
-   //printf("replacestr(%s, %s, %s)\n", line, search, replace);
-   if ((sp = strstr(line, search)) == NULL) {
-      return(0);
-   }
-   count = 1;
-   int sLen = strlen(search);
-   int rLen = strlen(replace);
-   if (sLen > rLen) {
-      // move from right to left
-      char *src = sp + sLen;
-      char *dst = sp + rLen;
-      while((*dst = *src) != '\0') { dst++; src++; }
-   } else if (sLen < rLen) {
-      // move from left to right
-      int tLen = strlen(sp) - sLen;
-      char *stop = sp + rLen;
-      char *src = sp + sLen + tLen;
-      char *dst = sp + rLen + tLen;
-      while(dst >= stop) { *dst = *src; dst--; src--; }
-   }
-   memcpy(sp, replace, rLen);
-
-   count += replacestr(sp + rLen, search, replace);
-
-   return(count);
-}
 
 int set_interface_attribs (int fd, int speed, int parity)
 {
